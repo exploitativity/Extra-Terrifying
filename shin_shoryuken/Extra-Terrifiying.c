5be9d8c75ab9dbd7b5d37cfe184e81c95947adea
exploitativity <firerider521@gmail.com>
2014-07-24 09:36:53 -0700

    Added distance tracking
    found port numbers for ET sensors as well. need follow distance

--- shin_shoryuken/Extra-Terrifiying.c
+++ shin_shoryuken/Extra-Terrifiying.c
@@ -1,12 +1,27 @@
-int leftET=
-int rightET=
 
 
+int leftET=1;
+int rightET=3;
+int followDistance=;
 
 int main()
 {
+	set_analog_pullup(leftET,0);
+	set_analog_pullup(rightET,0);
 	
-	printf("what am I doing\n")
+	while(analog(leftET)<followDistance && analog(rightET)<followDistance)
+	{
+		motor(1,500);
+		motor(3,500);
+		msleep(500);
+	}
 	
+	while(analog(leftET)>followDistance && analog(rightET)<followDistance)
+	{
+		motor(1,-100);
+		motor(3,-100);
+		msleep(500);
+		
+	}
 	return 0;
 }
